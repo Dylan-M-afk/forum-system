379af38c0c48ff642e9c3485156d851d83ca4925
DylanM-afk <dylanm12340@gmail.com>
2023-12-04 23:15:02 -0330

    Add MiniDrawer component and implement localStorage caching for replies

--- client/src/components/Replies.js
+++ client/src/components/Replies.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
+import MiniDrawer from "./MiniDrawer";
+
 const Replies = () => {
   const [replyList, setReplyList] = useState([]);
   const [reply, setReply] = useState("");
@@ -12,22 +14,32 @@ const Replies = () => {
   useEffect(() => {
     const fetchReplies = () => {
       setLoading(true); // Set loading to true before fetching replies
-      fetch("http://localhost:4000/api/thread/replies", {
-        method: "POST",
-        body: JSON.stringify({
-          id,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setReplyList(data.replies);
-          setTitle(data.title);
+
+      // Check if replies exist in localStorage for the current page
+      const storedReplies = localStorage.getItem(`replies_${id}`);
+      if (storedReplies) {
+        setReplyList(JSON.parse(storedReplies));
+        setLoading(false);
+      } else {
+        fetch("http://localhost:4000/api/thread/replies", {
+          method: "POST",
+          body: JSON.stringify({
+            id,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         })
-        .catch((err) => console.error(err))
-        .finally(() => setLoading(false)); // Set loading to false after fetching replies
+          .then((res) => res.json())
+          .then((data) => {
+            setReplyList(data.replies);
+            setTitle(data.title);
+            // Save replies to localStorage for the current page
+            localStorage.setItem(`replies_${id}`, JSON.stringify(data.replies));
+          })
+          .catch((err) => console.error(err))
+          .finally(() => setLoading(false)); // Set loading to false after fetching replies
+      }
     };
 
     fetchReplies();
@@ -60,40 +72,55 @@ const Replies = () => {
   };
 
   return (
-    <main className='replies'>
-      <h1 className='repliesTitle'>{title}</h1>
+    <MiniDrawer>
+      <main className="replies">
+        <button
+          onClick={() => navigate("/dashboard")}
+          style={{
+            backgroundColor: "#f50057",
+            color: "#fff",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "4px",
+            cursor: "pointer",
+          }}
+        >
+          Go Back
+        </button>
+        <h1 className="repliesTitle">{title}</h1>
 
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <form className='modal__content' onSubmit={handleSubmitReply}>
-          <label htmlFor='reply'>Reply to the thread</label>
-          <textarea
-            rows={5}
-            value={reply}
-            onChange={(e) => setReply(e.target.value)}
-            type='text'
-            name='reply'
-            className='modalInput'
-          />
-          <button className='modalBtn'>SEND</button>
-        </form>
-      )}
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <form className="modal__content" onSubmit={handleSubmitReply}>
+            <label htmlFor="reply">Reply to the thread</label>
+            <textarea
+              rows={5}
+              value={reply}
+              onChange={(e) => setReply(e.target.value)}
+              type="text"
+              name="reply"
+              className="modalInput"
+            />
+            <button className="modalBtn">SEND</button>
+          </form>
+        )}
 
-      {!loading && (
-        <div className='thread__container'>
-          {replyList &&
-            replyList.map((reply) => (
-              <div className='thread__item' key={reply.id}>
-                <p>{reply.text}</p>
-                <div className='react__container'>
-                  <p style={{ opacity: "0.5" }}>by {reply.name}</p>
+        {!loading && (
+          <div className="thread__container">
+            {replyList &&
+              replyList.map((reply) => (
+                <div className="thread__item" key={reply.id}>
+                  <p>{reply.text}</p>
+                  <div className="react__container">
+                    <p style={{ opacity: "0.5" }}>by {reply.name}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
-        </div>
-      )}
-    </main>
+              ))}
+          </div>
+        )}
+      </main>
+    </MiniDrawer>
   );
 };
 
